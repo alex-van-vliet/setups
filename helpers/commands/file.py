476b862341e10e30749d901be2b51bab0ba6dd30
Alex van Vliet <alex.van-vliet@epita.fr>
2020-12-05 13:32:42 +0100

    feat(file): add destination

--- helpers/commands/file.py
+++ helpers/commands/file.py
@@ -1,5 +1,6 @@
-from shutil import copy
-from typing import TYPE_CHECKING
+from pathlib import Path
+from shutil import copyfile
+from typing import TYPE_CHECKING, Optional
 
 from helpers.commands.abstract_command import AbstractCommand
 
@@ -16,12 +17,16 @@ class File(AbstractCommand):
         Setup the parser
         """
         parser.add_argument('file')
+        parser.add_argument('--destination', default=None)
 
-    def __call__(self, runner: 'Runner', file: str, **kwargs):
+    def __call__(self, runner: 'Runner', file: str, destination: Optional[str], **kwargs):
         """
         Call the command
         :param runner: The runner
         :param file: The path to the file
+        :param destination: Where to copy the file, based on its name if not present
         """
+        destination = destination if destination else file
         file = runner.get_directory() / file
-        copy(file, ".")
+        destination = Path.cwd() / destination
+        copyfile(file, destination)
