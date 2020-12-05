6f42fdfe693ee2cfa842d8f4a3270cae5e1c6754
Alex van Vliet <alex.van-vliet@epita.fr>
2020-12-05 14:31:48 +0100

    feat: allow to specify where to create the setup

--- commands/run.py
+++ commands/run.py
@@ -1,4 +1,7 @@
 import sys
+from contextlib import contextmanager
+from os import makedirs, chdir
+from pathlib import Path
 
 from commands.command import Command
 from helpers.setups import setup as setup_fun
@@ -14,18 +17,32 @@ class RunCommand(Command):
         :param parser: The parser
         """
         parser.add_argument('setup', help='the setup to run')
+        parser.add_argument('directory', help='where to run the setup', default='.', nargs='?')
 
-    def __call__(self, setup, **kwargs):
+    def __call__(self, setup: str, directory: str, **kwargs):
         """
         Run the run command
         :param setup: The name of the setup
         :param kwargs: The arguments
         :return: Zero if there was no error, one otherwise
         """
-        try:
-            setup_fun(setup)
-        except ValueError as e:
-            print(str(e), file=sys.stderr)
-            return 1
+        destination = Path(directory).resolve()
+        makedirs(destination, exist_ok=True)
+
+        @contextmanager
+        def cd(path):
+            cwd = Path.cwd()
+            try:
+                chdir(path)
+                yield path
+            finally:
+                chdir(cwd)
+
+        with cd(destination):
+            try:
+                setup_fun(setup)
+            except ValueError as e:
+                print(str(e), file=sys.stderr)
+                return 1
 
         return 0
